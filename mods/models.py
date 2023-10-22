085d16f1b104c751ebd689ac9075f27a9eaec825
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 18:07:23 +0300

    Add files via upload

---
+++ mods/models.py
@@ -0,0 +1,11 @@
+from flask_login import UserMixin
+
+from . import db
+
+
+class UserAuth(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    group = db.Column(db.String(255), nullable=False)
+    b24_id = db.Column(db.Integer)
