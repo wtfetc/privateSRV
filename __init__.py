b30e288ac63a702653846d7970af867081e9d843
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 14:52:02 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-
+'''
 sentry_sdk.init(
     dsn="https://5a6b1a7f3d23458fa751de233db25cd0@o4505186652717056.ingest.sentry.io/4505186654289920",
     integrations=[
@@ -16,7 +16,7 @@ sentry_sdk.init(
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0
 )
-
+'''
 app = Flask(__name__)
 app.secret_key = 'some very secret phrase for web_app_4dk'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////root/web_app_4dk/web_app_4dk/DataBase.db'
