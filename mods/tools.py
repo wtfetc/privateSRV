780c446686912b52bd229066a55388ec34e7a534
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 20:26:33 +0300

    Update tools.py

--- mods/tools.py
+++ mods/tools.py
@@ -8,7 +8,7 @@ from mods.modules import authentication
 
 def send_bitrix_request(method: str, data: dict):
     try:
-        print(requests.post(f"{authentication('Bitrix')}{method}", json=data).json()['result'])
+        #print(requests.post(f"{authentication('Bitrix')}{method}", json=data).json()['result'])
         return requests.post(f"{authentication('Bitrix')}{method}", json=data).json()['result']
     except KeyError:
         print("eeeee")
