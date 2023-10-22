20cf757d7df261efd3c46d2eef22b693729ea61a
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 14:05:35 +0300

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -3,10 +3,9 @@ import base64
 
 def authentication(key):
     credentials = {
-        'Bitrix': 'IGh0dHBzOi8vdmM0ZGsuYml0cml4MjQucnUvcmVzdC8xLzYwZHl3d3lsZ3Zkcm11bG8v',
-        'Google': 'Yml0cml4MjQtZGF0YS1zdHVkaW8tMjI3OGM3YmZiMWE3Lmpzb24=',
-        'Chat-bot': 'aHR0cHM6Ly92YzRkay5iaXRyaXgyNC5ydS9yZXN0LzEvYWZzYmttMzB3MWlwN2Mxei8K',
-        'Checko': 'ak13N0NJSUlKdE9LU05VYg=='
+        'Bitrix': 'aHR0cHM6Ly9iMjQtbnd2dnJ1LmJpdHJpeDI0LnJ1L3Jlc3QvMS9pNXZlYXZmcDlpdGZydHR2Lw==',
+       # 'Google': 'Yml0cml4MjQtZGF0YS1zdHVkaW8tMjI3OGM3YmZiMWE3Lmpzb24=',
+      #  'Chat-bot': 'aHR0cHM6Ly92YzRkay5iaXRyaXgyNC5ydS9yZXN0LzEvYWZzYmttMzB3MWlwN2Mxei8K',
+     #   'Checko': 'ak13N0NJSUlKdE9LU05VYg=='
     }
     return base64.b64decode(credentials[key]).decode('utf-8')
-
