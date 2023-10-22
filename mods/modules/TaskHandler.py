dfacb99ae0133f3e10ce1175e2d56599f56c6dac
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 19:04:05 +0300

    Update TaskHandler.py

--- mods/modules/TaskHandler.py
+++ mods/modules/TaskHandler.py
@@ -37,7 +37,7 @@ def fill_task_title(req, event):
         if not contact_crm:
             return
         contact_crm = contact_crm[0][2:]
-         print(contact_crm)
+        print(contact_crm)
         contact_companies = list(map(lambda x: x['COMPANY_ID'], send_bitrix_request('crm.contact.company.items.get', {'id': contact_crm})))
         if not contact_companies:
             return
