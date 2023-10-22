1a6f9659c723f4959825e237279e96f4cc540d6e
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 20:27:08 +0300

    Update authentication.py

--- mods/modules/authentication.py
+++ mods/modules/authentication.py
@@ -2,7 +2,7 @@ import base64
 
 
 def authentication(key):
-    credentials = {
+   credentials = {
         'Bitrix': 'aHR0cHM6Ly92YzFjLmJpdHJpeDI0LnJ1L3Jlc3QvNDc5L21qbWRpNXczd3ZsOWpvNWcvCg==',
        # 'Google': 'Yml0cml4MjQtZGF0YS1zdHVkaW8tMjI3OGM3YmZiMWE3Lmpzb24=',
       #  'Chat-bot': 'aHR0cHM6Ly92YzRkay5iaXRyaXgyNC5ydS9yZXN0LzEvYWZzYmttMzB3MWlwN2Mxei8K',
