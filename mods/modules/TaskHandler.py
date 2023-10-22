1043e3b1f2f90b1f162258b3472dc3c9c7d0ea63
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 19:05:52 +0300

    Update TaskHandler.py

--- mods/modules/TaskHandler.py
+++ mods/modules/TaskHandler.py
@@ -15,6 +15,7 @@ def fill_task_title(req, event):
         'select': ['*', 'UF_*']
     })
     if not task_info or 'task' not in task_info or not task_info['task']: # если задача удалена или в иных ситуациях
+        print("0")
         return
     task_info = task_info['task']
    # task_registry(task_info, event)
