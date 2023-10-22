a1c0d0c460aa9ff7e53759e526df547c03e8c818
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 20:49:47 +0300

    Update TaskHandler.py

--- mods/modules/TaskHandler.py
+++ mods/modules/TaskHandler.py
@@ -12,7 +12,7 @@ def fill_task_title(req, event):
     BitrixQ = 'aHR0cHM6Ly92YzFjLmJpdHJpeDI0LnJ1L3Jlc3QvNDc5L21qbWRpNXczd3ZsOWpvNWcvCg=='
     webhook = base64.b64decode(BitrixQ).decode('utf-8')
     print (webhook)
-    B = Bitrix(webhook)
+    b = Bitrix(webhook)
     task_id = req['data[FIELDS_AFTER][ID]']
     print(task_id)
     task_info = b.get_all(
