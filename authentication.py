b7be8602de48a49dfe2c46abc15c5e294ed94950
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-22 19:11:33 +0300

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -3,7 +3,7 @@ import base64
 
 def authentication(key):
     credentials = {
-        'Bitrix': 'aHR0cHM6Ly9iMjQtbnd2dnJ1LmJpdHJpeDI0LnJ1L3Jlc3QvMS9pNXZlYXZmcDlpdGZydHR2Lw==',
+        'Bitrix': 'aHR0cHM6Ly9iMjQtbnd2dnJ1LmJpdHJpeDI0LnJ1L3Jlc3QvMS9yOHl6bWJlNHo5OHJlM3ZiLw==',
        # 'Google': 'Yml0cml4MjQtZGF0YS1zdHVkaW8tMjI3OGM3YmZiMWE3Lmpzb24=',
       #  'Chat-bot': 'aHR0cHM6Ly92YzRkay5iaXRyaXgyNC5ydS9yZXN0LzEvYWZzYmttMzB3MWlwN2Mxei8K',
      #   'Checko': 'ak13N0NJSUlKdE9LU05VYg=='
