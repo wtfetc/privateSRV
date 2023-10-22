314830571f636f41261cbcdc76497159d6db6c8b
wtfetc <ib09@list.ru>
2023-10-22 18:13:33 +0300

    2r3

---
+++ mods/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+# import sentry_sdk
+# from sentry_sdk.integrations.flask import FlaskIntegration
+
+'''
+sentry_sdk.init(
+    dsn="https://5a6b1a7f3d23458fa751de233db25cd0@o4505186652717056.ingest.sentry.io/4505186654289920",
+    integrations=[
+        FlaskIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
+
+'''
+app = Flask(__name__)
+app.secret_key = 'some very secret phrase for web_app_4dk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////root/application/mods/DataBase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
+import models
+import routes
+
+
+
