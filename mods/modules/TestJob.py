ad53ba16342cdedb86e55928e828a06017b3a171
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-25 22:27:54 +0300

    Update TestJob.py

--- mods/modules/TestJob.py
+++ mods/modules/TestJob.py
@@ -1,9 +1,15 @@
+import json
+import base64
+from mods.tools import send_bitrix_request
+from fast_bitrix24 import Bitrix
+webhook = "https://vc1c.bitrix24.ru/rest/479/mjmdi5w3wvl9jo5g/"
+b = Bitrix(webhook)
 
 
-
-
-
-
-b.call('im.notify.system.add', {
-        'USER_ID': 479,
-        'MESSAGE': f'Элементы РТиУ заполнены'})
+def test_job(req, event=None):
+    try:
+       b.call('im.notify.system.add', {
+        'USER_ID': req['userid'],
+        'MESSAGE': f'вам поставлена задача'})
+    except:
+        return
