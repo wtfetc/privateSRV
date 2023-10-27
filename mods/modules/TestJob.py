8c4bad9a9f4c79aeda4865a9eee4b54023af2a47
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-10-27 20:02:58 +0300

    Update TestJob.py

--- mods/modules/TestJob.py
+++ mods/modules/TestJob.py
@@ -15,8 +15,12 @@ def test_job(req, event=None):
        useridformatted = str([req['userid']])
        useridformatted = useridformatted[7:(len(useridformatted)-2)]
        print (useridformatted)
-       message_text =  f'\nвам поставлена задача' \
-                       f'\n текст' 
+       message_text =  f'Внимание! У клиента uiiu заканчивается оплата по договору. Пожалуйста, свяжитесь с клиентом.\n' \
+                       f'https://vc4dk.bitrix24.ru/crm/deal/details/' \
+                       f'\n' \
+                       f'Внимание! У клиента uiiu заканчивается оплата по договору. Пожалуйста, свяжитесь с клиентом.\n' \
+                       f'https://vc4dk.bitrix24.ru/crm/deal/details/' \
+                       f'\n'
        b.call('im.notify.system.add', {
         'USER_ID': useridformatted,
         'MESSAGE':  message_text
