3a6a75f08ffa2b11907ed9f2cfcbc97892d22472
wtfetc <148685666+wtfetc@users.noreply.github.com>
2023-11-12 13:02:45 +0300

    Update SendDealChangedUserMessage.py

--- mods/chat_bot/SendDealChangedUserMessage.py
+++ mods/chat_bot/SendDealChangedUserMessage.py
@@ -1,1 +1,8 @@
+from web_app_4dk.chat_bot.SendMessage import bot_send_message
 
+
+def send_deal_changed_user_message(req):
+    bot_send_message({
+        'dialog_id': '479',
+        'message': f'Ответственным за сделку {req["deal_name"]} https://vc1с.bitrix24.ru/crm/deal/details/{req["deal_id"]}/ был назначен {req["user_after"]}'
+    })
