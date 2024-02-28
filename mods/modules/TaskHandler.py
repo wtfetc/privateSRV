1ad7b4604c34e339f33c5156241201bf257866f5
wtfetc <148685666+wtfetc@users.noreply.github.com>
2024-02-28 11:29:35 +0300

    Update TaskHandler.py

--- mods/modules/TaskHandler.py
+++ mods/modules/TaskHandler.py
@@ -149,13 +149,13 @@ def fill_task_title(req, event):
         old_aud = task_info['auditors']
         old_aud.append('491')
         print(old_aud)
-        print(task_info)
+        print(user_info)
 
         
         if task_info['groupId'] in ['119']: # если это определенная группа
 
             # если ответственный за компанию это НЕ постановщик или ответственный
-            if (task_info['responsibleId'] not in user_info[0]['ID']) or (task_info['createdBy'] not in user_info[0]['ID']):
+            if (task_info['responsibleId'] not in user_info['ID']) or (task_info['createdBy'] not in user_info['ID']):
                 old_aud.append(company_info['ASSIGNED_BY_ID']) # добавляем ответственного за компанию в наблюдатели
                 print(old_aud)
 
